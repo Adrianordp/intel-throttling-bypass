d19708bfc2f359684531fb749211503d24236210
Adrianordp <adrianordp@alu.ufc.br>
2024-05-11 19:14:01 -0400

    doc[startup-script.sh]: include comments

--- startup-script.sh
+++ startup-script.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Load 'msr' module
 /sbin/modprobe msr
+
+# Read MSR_POWER_CTL register values in hexa
 msr_hexa=`/usr/sbin/rdmsr 0x1FC`
+
+# Converts to hexa string in format 0x??
 msr_hexa_string='0x'$msr_hexa
+
+# Resets ENABLE_BIDIR_PROCHOT (bit 0)
 prochot_disabled_value=$(($msr_hexa_string&0xFFFFFE))
+
+# Write intruction to bypass PROCHOT to all CPUs
 /usr/sbin/wrmsr 0x1FC $prochot_disabled_value
